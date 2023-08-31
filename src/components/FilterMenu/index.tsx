0d6275bb676ee2fc95c9681df0e53ce18b3839d1
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-08-31 11:11:44 -0300

    m

--- src/components/FilterMenu/index.tsx
+++ src/components/FilterMenu/index.tsx
@@ -1,3 +1,4 @@
+import { filterInfo } from '../../modules/filterInfo';
 import { Container, Filter, FilterItem, Carousel } from './styles';
 
 export const FilterMenu = () => {
@@ -5,26 +6,12 @@ export const FilterMenu = () => {
     <Container>
       <Carousel>
         <Filter>
-          <FilterItem>
-            <img src="https://i.pinimg.com/564x/ce/4e/68/ce4e6892298ce665e4af40221fa4974c.jpg" alt="Filter Item" />
-            <h3>Menu principal</h3>
-          </FilterItem>
-          <FilterItem>
-            <img src="https://i.pinimg.com/564x/ce/4e/68/ce4e6892298ce665e4af40221fa4974c.jpg" alt="Filter Item" />
-            <h3>Combos Menu</h3>
-          </FilterItem>
-          <FilterItem>
-            <img src="https://i.pinimg.com/564x/ce/4e/68/ce4e6892298ce665e4af40221fa4974c.jpg" alt="Filter Item" />
-            <h3>Happy Hour</h3>
-          </FilterItem>
-          <FilterItem>
-            <img src="https://i.pinimg.com/564x/ce/4e/68/ce4e6892298ce665e4af40221fa4974c.jpg" alt="Filter Item" />
-            <h3>Para a Criançada</h3>
-          </FilterItem>
-          <FilterItem>
-            <img src="https://i.pinimg.com/564x/ce/4e/68/ce4e6892298ce665e4af40221fa4974c.jpg" alt="Filter Item" />
-            <h3>Bebidas</h3>
-          </FilterItem>
+          {filterInfo.map((item) => (
+            <FilterItem key={item.id}>
+              <img src={item.image} alt={item.name} />
+              <h3>{item.name}</h3>
+            </FilterItem>
+          ))}
         </Filter>
       </Carousel>
     </Container>
---
+++ src/modules/filterInfo.ts
@@ -0,0 +1,27 @@
+export const filterInfo = [
+  {
+    id: 1,
+    image: '/filter-images/filter.png',
+    name: 'Menu Principal'
+  },
+  {
+    id: 2,
+    image: '/filter-images/filter2.png',
+    name: 'Combos Menu'
+  },
+  {
+    id: 3,
+    image: '/filter-images/filter3.png',
+    name: 'Happy Hour'
+  },
+  {
+    id: 4,
+    image: '/filter-images/filter4.png',
+    name: 'Para a Criançada'
+  },
+  {
+    id: 5,
+    image: '/filter-images/filter5.png',
+    name: 'Bebidas'
+  }
+];
